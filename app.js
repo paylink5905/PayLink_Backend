9de0810dd516733279e1444e8e79ced210db712e
AniketKumar5155 <your_email@example.com>
2026-06-19 03:29:01 +0530

    updating mds

--- app.js
+++ app.js
@@ -1,26 +1,24 @@
 require('dotenv').config();
 const express = require('express');
-const serverless = require('serverless-http');
 const cors = require('cors');
-const errorMiddleware = require('./middlewares/error.middleware');
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
 const authRoutes = require('./routes/auth.routes');
-const serviceRoutes = require('./routes/services.routes')
+const serviceRoutes = require('./routes/services.routes');
 const paymentLinkRoutes = require('./routes/payment_links.routes');
 
 app.use('/api/auth', authRoutes);
 app.use('/api/services', serviceRoutes);
 app.use('/api/payment-links', paymentLinkRoutes);
 
+const errorMiddleware = require('./middlewares/error.middleware');
 app.use(errorMiddleware);
 
 const PORT = process.env.PORT || 3000;
-
-module.exports.handler = serverless(app);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
